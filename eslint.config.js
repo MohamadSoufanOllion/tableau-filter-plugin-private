7b3fa760704488b3cd079bd07c6268774ed44a52
MohamadSoufan <ms584@evansville.edu>
2025-05-13 12:58:06 -0400

    added package.json

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,19 @@
 // eslint.config.js
-import js from "@eslint/js";
-import security from "eslint-plugin-security";
-import noUnsanitized from "eslint-plugin-no-unsanitized";
+const js = require("@eslint/js");
+const security = require("eslint-plugin-security");
+const noUnsanitized = require("eslint-plugin-no-unsanitized");
 
 export default [
   js.configs.recommended,
   {
     plugins: {
       security,
-      noUnsanitized
+      noUnsanitized,
     },
     rules: {
       ...security.configs.recommended.rules,
       ...noUnsanitized.configs.recommended.rules,
-      "security/detect-object-injection": "off" // Example: Disable specific rules if necessary
-    }
-  }
+      "security/detect-object-injection": "off", // Example: Disable specific rules if necessary
+    },
+  },
 ];
